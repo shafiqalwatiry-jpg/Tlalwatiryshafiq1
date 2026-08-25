c7372e58a71d40e574d7be0fee8de2885cac4b5f
hdhdhdhhdhdhhh1-byte <hdhdhdhhdhdhhh1@gmail.com>
2026-08-26 01:19:21 +0300

    Revert "refactor: migrate to FragmentActivity and cleanup" This reverts commit eacdfcebf96edf9354ed659d12053a7d5b0c3c66.
    wq
    q
    wq
    
    wq

--- android/app/src/main/java/com/tilawatak/MainActivity.kt
+++ android/app/src/main/java/com/tilawatak/MainActivity.kt
@@ -14,6 +14,7 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -23,12 +24,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentActivity
 import com.tilawatak.notification.NotificationHelper
 import com.tilawatak.notification.WebAppInterface
 import java.util.concurrent.Executor
 
-class MainActivity : FragmentActivity() {
+class MainActivity : ComponentActivity() {
 
     private var filePathCallback: ValueCallback<Array<Uri>>? = null
 
